e91595ffcfa9e5246250634f11cf66a2868c3365
aluya ehis <aluyaehis8@gmail.com>
2024-03-29 05:46:34 -0700

    space rocket

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratio: 1/2)' },
+      }
+    },
+  },
+  plugins: [],
+}
+
